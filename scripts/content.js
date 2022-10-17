b5873a8517d94e8bd531393aad6575a96c9e1e8c
Theodor Borgman <tb222yz@student.lnu.se>
2022-10-17 23:10:29 +0200

    Fix data fetch issue with promise.

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,16 @@
 function setBodyData(capturedUrl) {
   const bodyElement = document.body.innerHTML
   chrome.storage.local.set({
-    elementalkey: bodyElement
+    elementalkey: bodyElement,
   })
-  console.log('Data saved from: ' + capturedUrl)
+  console.log("Data saved from: " + capturedUrl)
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    const currentUrl = window.location.href
-    setBodyData(currentUrl)
-    chrome.runtime.sendMessage({elementalkey: currentUrl}, function(response) {
-      console.log('Captured URL Body data to local storage key = elementalkey')
-    })
-    return true
-  }
-)
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  const currentUrl = window.location.href
+  setBodyData(currentUrl)
+  chrome.runtime.sendMessage({ elementalkey: currentUrl }, function (response) {
+    console.log("Captured URL Body data to local storage key = elementalkey")
+  })
+  return true
+})
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,25 +1,57 @@
+import { getBodyDataFromStorage } from "./dataCapture.js"
+import * as elementScraper from 'element-scraper'
 
-import { updateDataFromField } from "./dataCaptured.js"
+// Only global variable in extension space.
+let bodyData = ""
 
-let caputredData
+// Event listeners
 
+// button for triggering data save to chrome local storage.
+const getData = document.getElementById("get-data")
+getData.addEventListener("click", function () {
+  triggerUrlDataCapture()
+  getDataCapture()
+  return true
+})
+
+// button for manipulating data
+const manipulateData = document.getElementById("manipulate-data")
+manipulateData.addEventListener("click", function () {
+  formatData()
+  return true
+})
+
+// Call capture function on client side.
 function triggerUrlDataCapture() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {message: "capture"}, function(response) {
-      console.log("sent")
-    })
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { message: "capture" },
+      function (response) {}
+    )
   })
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    const dataFrom = sender.tab.url
-    caputredData = updateDataFromField(dataFrom)
-    return true
-  }
-)
-
-const getData = document.getElementById("get_data")
-getData.addEventListener("click", function() {
-  triggerUrlDataCapture()  
+// show message that data has been captured, from specific url.
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  const fromUrl = sender.tab.url
+  logDataFetchMessage(fromUrl)
+  return true
 })
+
+/**
+ * Show the url that data was fetched from.
+ *
+ * @param {string} dataFromUrl URL string
+ */
+function logDataFetchMessage(dataFromUrl) {
+  const dataFromField = document.getElementById("data-from")
+  dataFromField.innerHTML = "Data Captured from: " + dataFromUrl
+}
+
+export { getBodyDataFromStorage, logDataFetchMessage }
+
+// format data.
+async function getDataCapture() {
+  bodyData = await getBodyDataFromStorage()
+}
